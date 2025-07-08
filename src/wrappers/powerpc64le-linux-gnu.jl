27cf756c8f9f911a60ef899897e33caa5a22edec
jlbuild <juliabuildbot@gmail.com>
2025-07-08 20:02:49 +0000

    libavif_jll build 1.3.0+0

--- src/wrappers/powerpc64le-linux-gnu.jl
+++ src/wrappers/powerpc64le-linux-gnu.jl
@@ -7,10 +7,11 @@ using Zlib_jll
 using dav1d_jll
 using libaom_jll
 using libpng_jll
+using libyuv_jll
 JLLWrappers.@generate_wrapper_header("libavif")
 JLLWrappers.@declare_library_product(libavif, "libavif.so.16")
 function __init__()
-    JLLWrappers.@generate_init_header(JpegTurbo_jll, XML2_jll, Zlib_jll, dav1d_jll, libaom_jll, libpng_jll)
+    JLLWrappers.@generate_init_header(JpegTurbo_jll, XML2_jll, Zlib_jll, dav1d_jll, libaom_jll, libpng_jll, libyuv_jll)
     JLLWrappers.@init_library_product(
         libavif,
         "lib/libavif.so",
